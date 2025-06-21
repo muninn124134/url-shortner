96c89907e2fe73bd4acede15707cea4c685c5007
muninn124134 <pedrocks127@gmail.com>
2025-06-21 11:23:28 -0300

    feat: create url model

---
+++ src/main/java/com/pedroAlencar/url_shortner/model/urlModel.java
@@ -0,0 +1,15 @@
+package com.pedroAlencar.url_shortner.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class urlModel {
+
+    private String longUrl;
+    private int lenght;
+    
+}
